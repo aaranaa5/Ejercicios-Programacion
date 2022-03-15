0e3f8576f38dc17966514e8fb65402ee7da86ed1
Abraham A <mantenimiento.arana@gmail.com>
2022-03-14 20:50:59 -0500

    solo para 30 dias

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof texto);
+console.log(typeof binario);
+console.log(typeof noDefinido);
+console.log(typeof nulo);
+console.log(typeof pi);
---
+++ variable.js
@@ -0,0 +1,7 @@
+let texto="Esta es una variable de texto";
+let binario= true;
+let noDefinido;
+let nulo=null;
+const pi=3.156162561;
+
+
